6dd4b065d72afaeb130aaa4ff4f303bf06cd8508
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2021-04-13 18:57:53 -0300

    First 15 numbers of the Fibonacci sequence.

---
+++ aula/fibonacci.m
@@ -0,0 +1,23 @@
+
+clc;
+clear all;
+
+anterior = 1;
+atual = 1;
+cont = 2;
+
+anterior
+atual
+
+while cont < 14;
+
+proximo = atual + anterior;
+
+proximo
+
+anterior = atual;
+atual = proximo;
+
+cont = cont + 1;
+
+end
